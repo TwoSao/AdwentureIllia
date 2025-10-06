886aa6662306253d6b79a0e828e3b57f1961a844
Illia Blahun <illyablagun@gmail.com>
2025-10-06 10:00:28 +0300

    Lõpetatud ülesanne 33 failiga

--- 32_33_funktsioonid.sql
+++ 32_33_funktsioonid.sql
@@ -1,4 +1,5 @@
---funktsioonid
+use AdventureWorksDW2019
+--funktsioonid 32
 select * from DimEmployee
 
 -- Tabelisisev‰‰rtusega funktsioon e Inline Table Valued function (ILTVF) koodin‰ide: 
@@ -10,6 +11,50 @@ return (select EmployeeKey, FirstName, Cast(BirthDate as Date) as DOB from dbo.D
 
 select * from fn_ILTVF_GeyEmployess();
 
+-- ’pilane teeb p‰ringu ja n‰itab koodi kirja ning tulemust pildi kujul.
 
 Create Function fn_MSTVF_GetEmployess()
-Returns @Table Table (EmployeeKey int, FirstName nvarchar(20), 
+Returns @Table Table (EmployeeKey int, FirstName nvarchar(20), BrithDate Date) 
+as
+Begin
+Insert into @Table
+Select EmployeeKey, FirstName, Cast(BirthDate as DATE)
+from DimEmployee
+return
+end
+
+Select * from fn_MSTVF_GetEmployess()
+--funktsioonid 33
+
+--’pilane teeb p‰ringu ja n‰itab koodi kirja ning tulemust pildi kujul.
+CREATE FUNCTION fn_GetEmployeeNameById (@ID INT)
+RETURNS NVARCHAR(20)
+AS
+BEGIN
+    RETURN (SELECT FirstName FROM DimEmployee WHERE EmployeeKey = @ID);
+END;
+
+-- ’pilane teeb p‰ringu ja n‰itab koodi kirja ning tulemust pildi kujul.
+EXEC sp_helptext 'fn_GetEmployeeNameById';
+SELECT dbo.fn_GetEmployeeNameById(1) AS EmployeeName;
+
+
+--’pilane teeb p‰ringu ja n‰itab koodi kirja ning tulemust pildi kujul.
+Alter Function fn_GetEmployeeNameById(@ID int)
+Returns nvarchar(20)
+with Encryption
+as
+begin
+return (Select FirstName from DimEmployee where EmployeeKey=@ID)
+end
+
+
+-- ’pilane teeb p‰ringu ja n‰itab koodi kirja ning tulemust pildi kujul.
+Alter function fn_GetEmployeeNameById( @ID int)
+returns nvarchar(20)
+With SchemaBinding
+as
+begin
+Return(Select FirstName from dbo.DimEmployee Where EmployeeKey=@ID)
+end
+
