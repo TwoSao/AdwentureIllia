581c4799015122b3bd3a0b9afad8acde7157925a
Illia Blahun <illyablagun@gmail.com>
2025-10-06 12:14:40 +0300

    Lisati unikaalsed ja mitte-unikaalsed indeksid tabelile DimEmployee ning lisati veerg City koos unikaalse piiranguga. Kasutati ka IGNORE_DUP_KEY valikut dubleeritud võtmete käsitlemiseks.

--- 35_Ineksid_serveris.sql
+++ 35_Ineksid_serveris.sql
@@ -27,3 +27,43 @@ On DimEmployee(FirstName)
 -- Võib olla mitu mitte-klastreeritud indeksit ühel tabelil.
 -- Mitte-klastreeritud indeks sisaldab näitajaid (pointereid) tegelike andmete asukohtadele.
 -- Ei mõjuta andmete füüsilist järjestust tabelis.
+
+
+
+-- 37. Unikaalne ja mitte-unikaalne indeks
+
+-- Kuvame olemasolevad indeksid tabelis DimEmployee
+EXECUTE sp_helpindex 'DimEmployee';
+
+-- Lisame esimese kirje tabelisse DimEmployee
+INSERT INTO DimEmployee(EmployeeKey, FirstName, LastName, Salary, Gender)
+VALUES(1, 'Mike', 'Sandoz', 4500, 'M');
+
+-- Lisame kaks kirjet tabelisse tblEmployee, kus Id väärtus on sama (1)
+-- See tekitab vea, kui Id on primaarvõti ja unikaalne
+INSERT INTO tblEmployee VALUES(1,'Mike', 'Sandoz',4500,'M');
+INSERT INTO tblEmployee VALUES(1,'John', 'Menco',2500,'M');
+
+-- Kuvame kõik read tabelist DimEmployee
+SELECT * FROM DimEmployee;
+
+-- Loome unikaalse mitte-klastreeritud indeksi veergudele FirstName ja LastName
+-- See keelab sama ees- ja perekonnanimega dubleeritud kirjed
+CREATE UNIQUE NONCLUSTERED INDEX UIX_DimEmployee_FirstName_LastName
+ON DimEmployee(FirstName, LastName);
+
+-- Lisame tabelile DimEmployee veeru City
+ALTER TABLE DimEmployee 
+ADD City VARCHAR(50);
+
+-- Lisame veerule City unikaalse piirangu (constraint)
+-- Selleks luuakse taga unikaalne mitte-klastreeritud indeks
+ALTER TABLE DimEmployee 
+ADD CONSTRAINT UQ_tblEmployee_City 
+UNIQUE NONCLUSTERED (City);
+
+-- Loome unikaalse indeksi veerule City koos IGNORE_DUP_KEY valikuga
+-- See valik lubab sisestada dubleeruvaid väärtusi, mis jäetakse ignoreeritud
+CREATE UNIQUE INDEX IX_tblEmployee_City
+ON DimEmployee(City)
+WITH IGNORE_DUP_KEY;
