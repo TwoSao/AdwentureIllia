ea2dc24756b5a0934abeecdb9e893538949cc25d
Illia Blahun <illyablagun@gmail.com>
2025-10-06 09:27:32 +0300

    tegvus mis tehtud: Õpilane teeb päringu ja näitab koodi kirja ning tulemust pildi kujul.

--- 32_33_funktsioonid.sql
+++ 32_33_funktsioonid.sql
@@ -10,3 +10,6 @@ return (select EmployeeKey, FirstName, Cast(BirthDate as Date) as DOB from dbo.D
 
 select * from fn_ILTVF_GeyEmployess();
 
+
+Create Function fn_MSTVF_GetEmployess()
+Returns @Table Table (EmployeeKey int, FirstName nvarchar(20), 
